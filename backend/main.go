22e088a612d0361746be19e16ef83b4659887f00
Humphrey Ahn <ahnsv@bc.edu>
2018-07-30 22:21:30 +0900

    aws credentials added, put a test table with json from asset folder

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,37 @@
 package backend
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/gin-gonic/gin"
 )
+
+// struct for products
+type Product struct {
+	name         string `json: "name"`
+	url          string `json: "url"`
+	price        int32  `json: "price"`
+	image        string `json: "image"`
+	is_available bool   `json: "availability"`
+	is_subable   bool   `json: "subscribabilty"`
+}
+
+func getProducts() []Product {
+	raw, err := ioutil.ReadFile("./asset/dummy.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var products []Product
+	json.Unmarshal(raw, &products)
+	return products
+}
 
 func main() {
 	config := &aws.Config{
@@ -17,9 +43,27 @@ func main() {
 
 	svc := dynamodb.New(sess)
 
-	input := &dynamodb.CreateTableInput{
-		AttributeDefinitions: []*dynamodb.AttributeDefinition{},
+	products := getProducts()
+
+	for _, product := range products {
+		av, err := dynamodbattribute.MarshalMap(product)
+
+		if err != nil {
+			fmt.Println("got error on marshalling map:")
+			log.Fatal(err)
+		}
+		input := &dynamodb.PutItemInput{
+			Product:   av,
+			TableName: aws.String("test_products"),
+		}
+		_, err := svc.PutItem(input)
+		if err != nil {
+			fmt.Println("got error on putting item")
+			log.Fatal(err)
+		}
+		fmt.Print("successfully added '", product.name)
 	}
+
 	router := gin.Default()
 
 	router.GET("/", Index)
@@ -33,12 +77,12 @@ func main() {
 }
 
 func Index(c *gin.Context) {
-
+	
 }
 func GetProducts(c *gin.Context) {
 
 }
-func AddProduct(c *gin.Context, product *product) {
+func AddProduct(c *gin.Context) {
 
 }
 func GetProductByID(c *gin.Context) {
