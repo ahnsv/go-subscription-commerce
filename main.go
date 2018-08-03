5a10d1be5c202cf0449b855843e6345868d5cd6e
ahnsv <ahnsv@bc.edu>
2018-08-03 14:53:05 +0900

    AWS credential struggling, added a few more directories
    - AWS new static credential works, but still struggling with shared
    credentials
    - Figured out how to organize packages and import them
    - TODO
        - Figure out how to connect shared credentials instead of static and
        unreliable hardcoding
        - How should I connect DB driven data with routers and use them in
        frontend
        - Still haven't started with iamport config too

--- main.go
+++ main.go
@@ -1,14 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"log"
-
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
-	"github.com/gocolly/colly"
+	"github.com/ahnsv/go-subscription-commerce/db"
 )
 
 /*
@@ -17,46 +10,5 @@ TODO: define a struct to contain the product data and be sent to the MongoDB sto
 */
 
 func main() {
-	sess, err := session.NewSession(&aws.Config{
-		Region:      aws.String("us-west-2"),
-		Credentials: credentials.NewSharedCredentials("/c/Users/안상태/go/src/github.com/ahnsv/colly/.aws/credentials.csv", "ahnsv"),
-	})
-	svc := dynamodb.New(sess)
-	// Test connection
-	req := &dynamodb.DescribeTableInput{
-		TableName: aws.String("url_name"),
-	}
-	result, err := svc.DescribeTable(req)
-	if err != nil {
-		fmt.Printf("%s", err)
-	}
-	table := result.Table
-	fmt.Printf("done", table)
-	// Instantiate default collector
-	c := colly.NewCollector(
-		// Visit only domains: birchbox.com
-		colly.AllowedDomains("www.birchbox.com"),
-	)
-
-	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
-		// Print link
-		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
-		// Visit link found on page
-		// Only those links are visited which are in AllowedDomains
-		c.Visit(e.Request.AbsoluteURL(link))
-	})
-
-	// Before making a request print "Visiting ..."
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL.String())
-	})
-
-	c.OnError(func(_ *colly.Response, err error) {
-		log.Println("Something went wrong:", err)
-	})
-
-	// Start scraping on birchbox's best sellers
-	c.Visit("https://www.birchbox.com/category/149")
+	db.Init()
 }
